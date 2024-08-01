b51d15cd4d891013731bbbcda4769668bc7e01fe
Maksym Zhuk <bitmaksum550@gmail.com>
2024-08-01 20:57:33 +0300

    initial commite

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require(`express`);
+const path = require(`path`);
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, `public`)))
+
+app.get(`/home`, (req, res)=>{
+    res.sendFile(path.join(__dirname, `public`, `index.html`))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server work op port ${PORT}`)
+});
