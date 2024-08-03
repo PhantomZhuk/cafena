25797521551f96b454588d5752bd7597b1e0a201
Maksym Zhuk <bitmaksum550@gmail.com>
2024-08-03 21:00:57 +0300

    add home page

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.get(`/home`, (req, res)=>{
     res.sendFile(path.join(__dirname, `public`, `index.html`))
 })
 
-
 app.listen(PORT, () => {
     console.log(`Server work op port ${PORT}`)
 });
