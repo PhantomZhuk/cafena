03c7d389e73cbcd254fe02aa7ffe162b8b8a0181
Maksym Zhuk <bitmaksum550@gmail.com>
2024-08-15 10:56:17 +0300

    add a button to the pages of popular products and best products

--- public/popularProduct/popularProduct.js
+++ public/popularProduct/popularProduct.js
@@ -9,3 +9,45 @@ $(`.fa-bars`).click(()=>{
         menuOpen = false;
     }
 });
+
+$(`.coffeeContainer1`).hover(()=>{
+        $(`.btnContainer1`).css(`display`, `flex`)
+},
+()=>{
+    $(`.btnContainer1`).css(`display`, `none`)
+})
+
+$(`.coffeeContainer2`).hover(()=>{
+    $(`.btnContainer2`).css(`display`, `flex`)
+},
+()=>{
+$(`.btnContainer2`).css(`display`, `none`)
+})
+
+$(`.coffeeContainer3`).hover(()=>{
+    $(`.btnContainer3`).css(`display`, `flex`)
+},
+()=>{
+$(`.btnContainer3`).css(`display`, `none`)
+})
+
+$(`.coffeeContainer4`).hover(()=>{
+    $(`.btnContainer4`).css(`display`, `flex`)
+},
+()=>{
+$(`.btnContainer4`).css(`display`, `none`)
+})
+
+$(`.coffeeContainer5`).hover(()=>{
+    $(`.btnContainer5`).css(`display`, `flex`)
+},
+()=>{
+$(`.btnContainer5`).css(`display`, `none`)
+})
+
+$(`.coffeeContainer6`).hover(()=>{
+    $(`.btnContainer6`).css(`display`, `flex`)
+},
+()=>{
+$(`.btnContainer6`).css(`display`, `none`)
+})
