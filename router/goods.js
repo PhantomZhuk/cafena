5141bd25532e9dfdb488ba218ce274bd05eb679d
Maksym Zhuk <bitmaksum550@gmail.com>
2024-11-10 16:41:03 +0200

    create a user.js file for user functions and delete unnecessary files

--- router/goods.js
+++ router/goods.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN;
@@ -27,145 +26,19 @@ const upload = multer({ storage: storage });
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    if (chatId != admin_chat_id) {
-        bot.sendMessage(chatId, 'Welcome! Select an action to continue.', {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: 'Start confirmation',
-                            callback_data: 'start_confirmation'
-                        }
-                    ]
-                ]
-            }
-        });
-    } else if (chatId == admin_chat_id) {
+    if (chatId == admin_chat_id) {
         bot.sendMessage(admin_chat_id, 'Administrator mode is enabled!');
-    }
-
-});
-
-bot.on('callback_query', (callbackQuery) => {
-    const msg = callbackQuery.message;
-    const chatId = msg.chat.id;
-
-    if (callbackQuery.data === 'start_confirmation') {
-        bot.sendMessage(chatId, `Please send your contact to confirm your order.`, {
-            reply_markup: {
-                keyboard: [
-                    [
-                        {
-                            text: 'Send my contact',
-                            request_contact: true
-                        }
-                    ]
-                ],
-                one_time_keyboard: true
-            }
-        });
-    } else if (callbackQuery.data === 'contacts') {
-        bot.sendMessage(chatId, 'Here are our contacts...');
-    }
-
-    bot.answerCallbackQuery(callbackQuery.id);
-});
-
-bot.on('contact', (msg) => {
-    const chatId = msg.chat.id;
-    const contact = msg.contact;
-
-    if (contact) {
-        let userPhoneNumber = contact.phone_number;
-        let formattedPhoneNumber = `+${userPhoneNumber}`;
-        fs.readFile(unconfirmedOrdersFilePath, 'utf8', (err, data) => {
-            if (err) {
-                console.log(err);
-                return bot.sendMessage(chatId, 'Unable to read product file.');
-            }
-            const orders = JSON.parse(data);
-
-            const ordersUnconfirmed = orders.filter(o => o.status === 'unconfirmed' && (o.phone === formattedPhoneNumber || o.phone === userPhoneNumber));
-
-            let message = '';
-            let totalPrice = 0;
-
-            for (let el of ordersUnconfirmed) {
-                for (let order of el.orders) {
-                    message += `Name: ${order.name}\nQuantity: ${order.quantity}\nPrice: ${order.price}$\n\n`;
-                    totalPrice += order.price * order.quantity;
-                }
-            }
-
-            if (message !== '') {
-                message += `Total Price: ${totalPrice}$`;
-                bot.sendMessage(chatId, 'Below is a list of your unconfirmed orders:');
-                bot.sendMessage(chatId, message, {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: 'Confirm order',
-                                    callback_data: `confirm_order_${formattedPhoneNumber}`
-                                }
-                            ]
-                        ]
-                    }
-                });
-            } else {
-                bot.sendMessage(chatId, 'No unconfirmed orders found.');
-            }
-        });
-    } else {
-        bot.sendMessage(chatId, 'Failed to receive contact. Please try again.');
     }
 });
 
 bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
-    const callbackData = callbackQuery.data;
 
-    if (callbackData.startsWith('confirm_order_')) {
-        let phone = callbackData.split('_')[2]
-        console.log(phone);
-
-        fs.readFile(unconfirmedOrdersFilePath, 'utf8', (err, fileData) => {
-            if (err) {
-                console.log(err);
-                return bot.sendMessage(chatId, 'Unable to read orders file.');
-            }
-
-            let orders = JSON.parse(fileData);
-            let userOrder = orders.find(o => o.phone === phone && o.status === 'unconfirmed');
-            console.log(userOrder);
-
-            if (userOrder) {
-                userOrder.status = 'confirmed';
-                let totalPrice = 0;
-                let adminMessage = '';
-
-                adminMessage = `User: ${userOrder.userName}\nPhone: ${userOrder.phone}\nOrder details:\n\n`;
-
-                for (let product of userOrder.orders) {
-                    adminMessage += `- Product Name: ${product.name}\n- Price: ${product.price}\n- Quantity: ${product.quantity}\n\n`;
-                    totalPrice += product.price * product.quantity;
-                }
-
-                adminMessage += `Total Price: ${totalPrice}$`;
-
-                bot.sendMessage(admin_chat_id, adminMessage);
-
-                fs.writeFile(unconfirmedOrdersFilePath, JSON.stringify(orders), 'utf8', (err) => {
-                    if (err) {
-                        console.log(err);
-                        return bot.sendMessage(chatId, 'Unable to update order status.');
-                    }
-
-                    bot.sendMessage(chatId, 'Your order has been confirmed!');
-                });
-            }
-        });
+    if (callbackQuery.data === 'contacts') {
+        bot.sendMessage(chatId, 'Here are our contacts...');
     }
+
+    bot.answerCallbackQuery(callbackQuery.id);
 });
 
 const ProductSchema = new mongoose.Schema({
